7e38f7df4f9b76db8604e9940eae45337b1c01c3
urvish0706 <patelurvish0706@gmail.com>
2024-12-28 21:40:26 +0530

    Day-2

--- Day1.java
+++ Day1.java
@@ -4,79 +4,87 @@ import java.util.*;
 
 public class Day1 {
 
-    public static void $(String topic){
-            System.out.println("\n--- "+topic+" ---");
-        }
-    
-        public static void main(String[] args) {
-            // --- Hello world !!! ---
-                $("Hello world");
-                
-                System.out.println("Hello Java!!!");
-                
-    
-            // --- Variables ---
-                $("Variables");
-                /* To assign Variable 
-                 * Syntax need to follow as: 
-                 * Datatype varName = VariableValue; */
+    public static void $(String topic) {
+        System.out.println("\n--- " + topic + " ---");
+    }
 
-                int number = 1234; 
-                String alphabet = "abc@d";
-                double dotNumeric = 2.50;
-                boolean anyCondition = true;
-    
-                System.out.println(number + " " +alphabet + " " + dotNumeric + " " + anyCondition);
-      
-            // --- Constant Varible ---
-                $("Constant Varible");
-                /* To make a constant variable 
-                 * have to add final keyword in variable assign,
-                 * final dataType varName = VarValue; */
+    public static void main(String[] args) {
+        // --- Hello world !!! ---
+        $("Hello world");
 
-                 final double pi = 3.14;
+        System.out.println("Hello Java!!!");
 
-                 System.out.println("Constant : " + pi);
+        // --- Variables ---
+        $("Variables");
+        /*
+         * To assign Variable
+         * Syntax need to follow as:
+         * Datatype varName = VariableValue;
+         */
 
-            // --- Data Types ---
-                $("Data Types");
-                /* Two types of DataTypes */
-                String type1 = "Primitive : byte , short , char , boolean , int , long , float , double ...";
-                String type2 = "Non-Primitive : string , array , class , object , interface ...";
-                
-                System.out.println(type1 + "\n" + type2);
+        int number = 1234;
+        String alphabet = "abc@d";
+        double dotNumeric = 2.50;
+        boolean anyCondition = true;
 
-            // --- Operation with Variables --- 
-                $("Operation with Variables");
-                
-                /* In String  */
-                    String word1 = "hello";
-                    String word2 = "World!";
-                    
-                    System.out.println("Word1 : " + word1 + "\n" + "Word2 : " + word2 + "\nConcated : " + (word1+word2) );
-                    
-                    String sentence = "Dsa With Java";
-                    System.out.println("Sentence in Variable : " + sentence);
-                
-                /* In Integer */
-                    System.out.println("\n--Java does not follow BODMAS in Maths operations.--");
-                    System.out.println("In java, *,/,% has more priority than +,- ");
+        System.out.println(number + " " + alphabet + " " + dotNumeric + " " + anyCondition);
 
-            // --- Input from User ---
-                $("Input from User");
-                    /* It is important to add 
-                     * import java.util.*;
-                     * to enable feature to take input. */
+        // --- Constant Varible ---
+        $("Constant Varible");
+        /*
+         * To make a constant variable
+         * have to add final keyword in variable assign,
+         * final dataType varName = VarValue;
+         */
 
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("Type something: ");
-                    String userInput = sc.nextLine(); 
+        final double pi = 3.14;
 
-                    //  next() -> Default, for String and Single character before space.
-                    //  nextLine() -> for String and Sentence with space, Integer , Double also.
-                    //  nextInt() -> Only for Integer, does NOT support Double.
+        System.out.println("Constant : " + pi);
 
-                    System.out.println("Your Input is : " + userInput);
-                    sc.close();
-    }   
+        // --- Data Types ---
+        $("Data Types");
+        /* Two types of DataTypes */
+        String type1 = "Primitive : byte , short , char , boolean , int , long , float , double ...";
+        String type2 = "Non-Primitive : string , array , class , object , interface ...";
+
+        System.out.println(type1 + "\n" + type2);
+
+        // --- Operation with Variables ---
+        $("Operation with Variables");
+
+        /* In String */
+        String word1 = "hello";
+        String word2 = "World!";
+
+        System.out.println("Word1 : " + word1 + "\n" + "Word2 : " + word2 + "\nConcated : " + (word1 + word2));
+
+        String sentence = "Dsa With Java";
+        System.out.println("Sentence in Variable : " + sentence);
+
+        /* In Integer */
+        System.out.println("\n--Java does not follow BODMAS in Maths operations.--");
+        System.out.println("In java, *,/,% has more priority than +,- ");
+
+        // --- Input from User ---
+        $("Input from User");
+        /*
+         * It is important to add
+         * import java.util.*;
+         * to enable feature to take input.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type something: ");
+        String userInput = sc.nextLine();
+
+        /* next() -> Default, for String and Single character before space.
+         * nextLine() -> for String and Sentence with space, Integer , Double also.
+         * nextInt() -> Only for Integer, does NOT support Double.
+         * nextDouble() -> Only for Double.
+         * nextBoolean() -> Only for True and False. 
+         */
+
+        System.out.println("Your Input is : " + userInput);
+        sc.close();
+    }
 }
